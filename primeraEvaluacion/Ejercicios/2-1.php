bdfc7666b64426231a427f5db20e8312ea9f7a42
Mario Silla Olmos <silolm.mail@gmail.com>
2019-09-23 19:43:17 +0200

    del 2-1 al 2-3

---
+++ primeraEvaluacion/Ejercicios/2-1.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+        
+        <div style = "color: red;" >
+            
+            <?php
+    
+           echo "&iexclhola mundo!";
+           
+           echo "<br>";
+           
+           $horaActual = date ('g:i:s a');
+           
+           echo "la hora actual es:  $horaActual";
+           
+            ?>
+            
+        </div>
+        
+    </body>
+
+</html>
---
+++ primeraEvaluacion/Ejercicios/2-3.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+
+        <div style = "color: red;" >
+
+            <?php
+    
+           echo "&iexclhola mundo!";
+
+           echo "<br>";
+
+           $horaActual = date ('H:i:s, l d/m/Y');
+
+           echo "la hora actual es:  $horaActual";
+
+            ?>
+
+        </div>
+
+    </body>
+
+</html>
