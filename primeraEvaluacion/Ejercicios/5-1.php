bd7386ab821b1b46a968a1de41f6501c1bb47dea
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-03 11:40:48 +0200

    azdasdas

--- primeraEvaluacion/Ejercicios/5-1.php
+++ primeraEvaluacion/Ejercicios/5-1.php
@@ -1,38 +1,90 @@
 <html>
+<head>
+    <title>
+        Simulador de paloma mensajera
+    </title>
+    <link rel="stylesheet" type="text/css" href="common.css"/>
+    <style type="text/css">
+        div.map {
+            float: left;
+            text-align: center;
+            border: 1px solid #666;
+            background-color: #fcfcfc;
+            margin: 5px;
+            padding: 1em;
+        }
+
+        span.casa, span.paloma {
+            font-weight: bold;
+            color: red;
+        }
+
+        span.aire {
+            color: blue;
+        }
+
+    </style>
+</head>
 <body>
 
-<table border="1" align="center">
+<h1>
     <?php
-    echo "<tr class='1'>";
-    $cabecera = "0";
-    while ($cabecera <= 10) {
-        echo "<th>";
-        echo $cabecera;
-        echo "</th>";
-        $cabecera++;
-    }
-    echo "</tr>";
-    $x = 1;
-    while ($x <= 10) {
-        echo "<tr>";
-        echo "<th>";
-        echo $x;
-        echo "</th>";
-        $x++;
 
-        $y = 1;
+    $tamañoMapa = 10;
 
-        while ($y <= 10) {
-            $multiplicacion = $x * $y;
-            echo "<td>";
-            echo "$x" . "*" . "$y = " . $multiplicacion;
-            echo "</td>";
-            $y++;
+    # Posicionar la paloma
+
+    do {
+        $casaX = rand(0, $tamañoMapa - 1);
+        $casaY = rand(0, $tamañoMapa - 1);
+        $palomaX = rand(0, $tamañoMapa - 1);
+        $palomaY = rand(0, $tamañoMapa - 1);
+    } while ((abs($casaX - $palomaX) < $tamañoMapa / 2) && (abs($casaY - $palomaY) < $tamañoMapa / 2));
+    # Con la línea precedente nos aseguramos de que la posición inicial de la paloma
+    # y su casa disten como mínimo la mitad del mapa. La función abs() devuelve el valor
+    # absoluto, esto es, la cifra sin signo.
+
+    do {
+        #Acercar la paloma a su casa
+        if ($palomaX < $casaX)
+            $palomaX++;
+
+        elseif ($palomaX > $casaX)
+            $palomaX--;
+
+
+        if ($palomaY < $casaY)
+            $palomaY++;
+        elseif ($palomaY > $casaY)
+            $palomaY--;
+
+
+        #Mostrar el mapa actual
+        echo '<div class="map" style="width: ' . $tamañoMapa . 'em;"><pre>';
+        # Recuérdese que con la etiqueta <pre> los saltos de línea que haya se reflejan en la pantalla
+
+        for ($y = 0; $y < $tamañoMapa; $y++) {
+            for ($x = 0; $x < $tamañoMapa; $x++) {
+                if ($x == $casaX && $y == $casaY) {
+                    echo '<span class="casa">+</span>'; //Casa
+                } elseif ($x == $palomaX && $y == $palomaY) {
+                    echo '<span class="paloma">%</span>'; #Paloma
+                } else {
+                    echo '<span class="aire">.</span>'; #Aire
+                }
+
+                echo ($x != $tamañoMapa - 1) ? " " : ""; #siempre se añade un carácter de espacio en cada celda, salvo al final.
+            }
+
+            echo "\n"; #Salto de línea. como se está dentro de un <pre>, se reflejará en la pantalla.
         }
-        echo "</tr>";
-    }
+
+        echo "</pre>palomax=$palomaX palomaY=$palomaY</div>\n";
+    } while ($palomaX != $casaX || $palomaY != $casaY);
+
     ?>
-</table>
+
+</h1>
 
 </body>
 
