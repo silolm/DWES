4d94c52a063fbdc71e8b8758e8bfb35d627ab37f
Mario Silla Olmos <silolm.mail@gmail.com>
2019-11-20 20:34:57 +0100

    polla

---
+++ segundaEvaluacion/Ejercicios/7-4.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="get" action="5.php">
+    <input type="number" name="operando1">
+    <select name="operador">
+        <option value="+">+
+        </option>
+        <option value="-">-
+        </option>
+        <option value="*">*
+        </option>
+        <option value="/">/
+        </option>
+    </select>
+    <input type="number" name="operando2">
+    <input type="submit" value="=">
+
+</form>
+
+<?php
+$operando1 = $_GET['operando1'];
+$operando2 = $_GET['operando2'];
+$operador = $_GET['operador'];
+$solucion = 0;
+
+if ($operador == "+")
+    $solucion = $operando1 + $operando2;
+else if ($operador == "-")
+    $solucion = $operando1 - $operando2;
+else if ($operador == "*")
+    $solucion = $operando1 * $operando2;
+else if ($operador == "/")
+    $solucion = $operando1 / $operando2;
+
+echo "<br> La solución es: " . $solucion;
+?>
+
+</body>
+</html>
+</html>
---
+++ segundaEvaluacion/Ejercicios/7-5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if (empty($_GET['1'])) {
+
+    ?>
+    <form method="get" action="5.php">
+
+        <input type="text" name="nombre" placeholder="Nombre">
+        <input type="hidden" name="1" value="1">
+        <input type="submit" value="enviar">
+
+    </form>
+    <?php
+} elseif ($_GET['1'] == '1') {
+    ?>
+    <form method="get" action="5.php">
+
+        <input type="text" name="vehiculo" placeholder="Vehículo">
+        <input type="hidden" name="1" value="2">
+        <input type="hidden" name="nombre" value="<?php echo $_GET['nombre'] ?>">
+        <input type="submit" value="enviar">
+
+    </form>
+
+    <?php
+} elseif ($_GET['1'] == '2')
+    echo 'Hola ' . $_GET['nombre'] . ', su vehículo es: ' . $_GET['vehiculo'];
+?>
+
+</body>
+</html>
+</html>
