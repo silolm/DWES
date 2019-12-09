04c5b603d3e0477100ac142efc57c2db07ad19b7
Mario Silla Olmos <silolm.mail@gmail.com>
2019-12-09 19:57:48 +0100

    apolla

--- segundaEvaluacion/Ejercicios/9-5.php
+++ segundaEvaluacion/Ejercicios/9-5.php
@@ -2,51 +2,58 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <style>
-        tr:nth-child(odd) {
-            background-color: gold;
-        }
-
-        tr:nth-child(even) {
-            background-color: lime;
-        }
-    </style>
     <title>Document</title>
 </head>
 <body>
 
-<form action="7-1.php" method="post">
-
-    Introduzca su nombre:
-    <br>
-    <input type="text" name="DNI" placeholder="DNI">
-    <br>
-    <input type="text" name="Nombre" placeholder="Nombre">
-    <br>
-    <input type="number" name="Sueldo" placeholder="Sueldo">
-    <br>
-    <input type="number" name="Plus" placeholder="Plus" readonly="readonly" value="250"> ➪ PLUS
-    <br>
-    <input type="submit" value="Enviar">
-    <br><br>
-
-</form>
 <?php
+# Si no hay una pulsación previa, muestra el formulario
+if (!isset ($_POST["boton"])) {
+    ?>
+
+    <form action="5.php" method="post">
+
+        <input type="text" name="Nombre" placeholder="Nombre">
+        <br>
+        <input type="number" name="DNI" placeholder="DNI">
+        <br>
+        <input type="number" name="Sueldo" placeholder="Sueldo">
+        <br>
+        <input type="number" name="Plus" placeholder="Plus" readonly="readonly" value="250"> ➪ PLUS
+        <br>
+        <input type="submit" value="Enviar">
+        <br><br>
+
+    </form>
+
+    <?php
+}
+
+$nombre = $_POST["Nombre"];
+$dni = $_POST["DNI"];
+$sueldo = $_POST["Sueldo"];
+$plus = $_POST["Plus"];
+
+
 $servidor = "localhost";
 $username = "mario";
 $password = "m1234";
 $basedatos = "bdprueba";
 
+
 $conn = mysqli_connect($servidor, $username, $password, $basedatos);
 if (!$conn)
     die("Conexión fallida: " . mysqli_connect_error());
 
-$consulta = "UPDATE empleados SET sueldo = sueldo + (sueldo * 10 / 100);";
+$consulta = "INSERT INTO empleados VALUES (" . $nombre . ",'" . $dni . "', " . $sueldo . " , " . $plus . ");";
 $result = mysqli_query($conn, $consulta);
 
-$consulta2 = "SELECT * FROM empleados";
-$result = mysqli_query($conn, $consulta2);
-
+if ($result == TRUE)
+    echo 'OK';
+else {
+    echo mysqli_error($conn);
+    die("Hubo un error");
+}
 
 mysqli_free_result($result);
 mysqli_close($conn);
